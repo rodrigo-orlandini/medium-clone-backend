992784c276d810912cb7c21535012435d6a00c6b
Rodrigo Orlandini <rodrigosorlandini@gmail.com>
2022-11-13 23:46:22 -0300

    Adding name and avatar of writer in /home response

--- src/routes/frontend.ts
+++ src/routes/frontend.ts
@@ -2,12 +2,17 @@ import { router } from '../lib/router';
 
 import { Post } from '../models/post';
 import { Topic } from '../models/topic';
+import { Writer } from '../models/writer';
 
 // /home endpoint to get all information that is used in home 
 router.get('/home', async (_req, res) => {
     // Getting posts ordered by number of likes
     const posts = await Post.findAll({
-        order: [["numOfLike", "DESC"]]
+        order: [["numOfLike", "DESC"]],
+        include: [{
+            model: Writer,
+            attributes: ['name', 'avatarUrl']
+        }]
     });
     
     const topics = await Topic.findAll();
