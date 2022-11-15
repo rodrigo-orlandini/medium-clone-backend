e87eb510166caba1209b9a80d08cdec6195383dd
Rodrigo Orlandini <rodrigosorlandini@gmail.com>
2022-11-15 00:55:18 -0300

    Reseting database

--- src/server.ts
+++ src/server.ts
@@ -5,7 +5,7 @@ import { bootstrap, syncStorage } from './helper/database';
 // Start server application
 app.listen(port, () => {
     // Used to start database in application
-    // bootstrap();
-    syncStorage();
+    bootstrap();
+    // syncStorage();
     console.log(`Server is running on http://localhost:${port}`);
 });
